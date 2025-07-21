5d0e9ca69432f13d0163fa4d9e71750cbe4a816f
Yeri <mail@yeripratama.com>
2025-07-21 10:59:09 +0700

    feat: set font and install magit

--- init.el
+++ init.el
@@ -1,6 +1,25 @@
+(set-face-attribute 'default nil :family "Monaspace Neon" :height 140)
 (load-theme 'modus-vivendi-tinted t)
 (menu-bar-mode -1)
 (tool-bar-mode -1)
 (scroll-bar-mode -1)
 (global-hl-line-mode 1)
 (setq org-agenda-files (directory-files-recursively "~/org" "\\.org$"))
+
+;; org
+(global-set-key (kbd "C-c a") #'org-agenda)
+(global-set-key (kbd "C-c c") #'org-capture)
+
+;; package
+(require 'package)
+
+(setq package-archives
+      '(("melpa" . "https://melpa.org/packages/")
+        ("gnu" . "https://elpa.gnu.org/packages/")))
+
+(package-initialize)
+
+(unless package-archive-contents
+  (package-refresh-contents))
+
+(package-install 'magit)
