e0fae19e10d14510803e68fc41567ed2d9da5d0a
Yeri <mail@yeripratama.com>
2025-07-22 09:35:07 +0700

    feat: usable config

--- init.el
+++ init.el
@@ -1,30 +1,48 @@
-(set-face-attribute 'default nil :family "Monaspace Neon" :height 130)
-(load-theme 'modus-vivendi-tinted t)
+;; custom file
+(setq custom-file "~/.emacs-custom.el")
+
+;; setup packages
+(require 'package)
+(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
+(package-initialize)
+
+(defvar my-packages
+  '(magit evil spacemacs-theme))
+
+(defun my-packages-installed-p ()
+  (cl-loop for p in my-packages
+           when (not (package-installed-p p)) do (cl-return nil)
+           finally (cl-return t)))
+
+(unless (my-packages-installed-p)
+  ;; check for new packages (package versions)
+  (package-refresh-contents)
+  ;; install the missing packages
+  (dolist (p my-packages)
+    (when (not (package-installed-p p))
+      (package-install p))))
+
+;; package config
+(require 'use-package)
+(use-package spacemacs-theme
+  :config
+  (load-theme 'spacemacs-dark t))
+
+(use-package evil
+  :init
+  (setq evil-want-C-u-scroll t)
+  :config
+  (evil-mode 1))
+
+(use-package org
+  :init
+  (setq org-agenda-files (directory-files-recursively "~/org" "\\.org$"))
+  :bind (("C-c a" . org-agenda)
+         ("C-c c" . org-capture)))
+
+;; options
 (menu-bar-mode -1)
 (tool-bar-mode -1)
 (scroll-bar-mode -1)
 (global-hl-line-mode 1)
-
-
-;; package
-(require 'package)
-(setq package-archives
-      '(("melpa" . "https://melpa.org/packages/")
-        ("gnu" . "https://elpa.gnu.org/packages/")))
-(package-initialize)
-(unless package-archive-contents
-  (package-refresh-contents))
-
-(package-install 'magit)
-
-;; vim
-(package-install 'evil)
-(require 'evil)
-(evil-mode 1)
-(define-key evil-normal-state-map (kbd "C-u") 'evil-scroll-up)
-
-
-;; org
-(setq org-agenda-files (directory-files-recursively "~/org" "\\.org$"))
-(global-set-key (kbd "C-c a") #'org-agenda)
-(global-set-key (kbd "C-c c") #'org-capture)
+(set-face-attribute 'default nil :family "Monaspace Neon" :height 160)
